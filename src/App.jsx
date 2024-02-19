1ff2ca33bc60f2323bdda911db1230020cc6dde1
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2024-02-19 13:31:07 +0200

    Project layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import SiteNav from "./components/SiteNav"
+import { Routes, Route } from "react-router-dom"
+import Homepage from "./views/Homepage"
+import AboutUS from "./views/AboutUS"
+import FAQ from "./views/FAQ"
+import Footer from "./components/Footer"
 const App = () => {
   return (
     <div>
-      {/* Nav */}
-      {/* Header */}
-      {/* partners */}
+      <SiteNav />
+
+
+      <Routes>
+        <Route path="/" Component={Homepage} />
+        <Route path="/about" Component={AboutUS} />
+        <Route path="/faq" Component={FAQ} />
+
+      </Routes>
+
+      <Footer />
     </div>
   )
 }
