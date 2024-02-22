716dfa5b7681fb3942a202cfaffb8c70284df278
Abdelrahman <abdelrahmansaad027@gmail.com>
2024-02-22 14:31:52 +0200

    setting up json server

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import SiteNav from "./layout/SiteNav"
 import { Routes, Route } from "react-router-dom"
 import Homepage from "./views/Homepage"
-import AboutUs from "./views/AboutUs"
+import AboutUs from "./views/AboutUS"
 import FAQ from "./views/FAQ"
 import Footer from "./layout/Footer"
 import ContactUs from "./views/ContactUs"
